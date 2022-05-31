2f35e7547d44adb50ef5f96708d59e67092e7d3d
Gabriel Mota da Câmara <gabriel.mota.cam@gmail.com>
2022-05-30 21:33:35 -0300

    avancando

---
+++ js/calcula-imc.js
@@ -0,0 +1,85 @@
+
+//.querySelector --> ferramenta para buscar uma informação desejada
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Calculadora IMC";
+
+var titulo2 = document.querySelector(".titulo2");
+titulo2.textContent = "Exemplos"
+
+
+//querySelectAll busca todos os elementos relacionados à pesquisa, em todas as linhas. Diferentemente do querySelector (que só busca uma linha)
+var pacientes = document.querySelectorAll(".paciente")
+
+//for (cria variável com valor = 0; estabelece a condição; incrementa)
+for (var i = 0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso= tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdGordura = paciente.querySelector(".info-gordura");
+    var gordura = tdGordura.textContent;
+    
+    
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    // //condição peso
+    if (peso <= 0 || peso >= 500){
+        console.log("peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        //é bom tratar de estilização no CSS, então se criou uma classe para linhas com erro de peso ou altura
+        //classList --> retorna uma lista de classes que a var possui.
+        //classList.add --> adiciona uma classe à var
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    // Condição Altura
+    if (altura <= 0 || altura >= 3){
+        console.log("altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida!";
+
+        //classList --> retorna uma lista de classes que a var possui.
+        //classList.add --> adiciona uma classe à var
+        paciente.classList.add("paciente-invalido") ;
+    }
+    if (alturaEhValida && pesoEhValido){
+        // var imc = tdImc.textContent = peso /(altura * altura);
+
+        //.toFixed(2) --> apresenta apenas duas casas decimais (escolher o número de casas)
+        // tdImc.textContent = imc.toFixed(2);
+        tdImc.textContent = calculaImc(peso,altura); //substitui o código acima pela função calculaImc
+    }
+    if (alturaEhValida == false && pesoEhValido == false){
+         tdImc.textContent = "Altura e Peso Inválidos";
+    }
+    
+}
+
+//var.addEventListener --> adiciona eventos. Neste caso faz o JS perceber quando algo é clicado.
+//quando alguém clica no título, aparece no console a mensagem da função
+
+// titulo.addEventListener("click", mostraMensagem);
+//quer capturar clicks, e depois aplica a function
+
+
+//para criar uma função, é só escrever inicialmente'function' e nomeá-la, como mostrado abaixo
+// function mostraMensagem(){
+//     console.log("Olá, eu fui clicado!");
+// }
+
+function calculaImc(peso,altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
---
+++ js/form.js
@@ -0,0 +1,83 @@
+
+//criando evento quando clicar no botão
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+
+    //evitar que a página recarregue quando clicar no botão (comportamento padrão)
+    //que é recarregar a página e enviar o form
+    //não queremos enviar formulário, queremos apenas adicionar dados na tabela...
+    event.preventDefault();
+    
+    
+    var form = document.querySelector("#form-adiciona");
+
+    //printar a var form.name.value (todo input tem name). Essa é a forma de acessar a caixa de input
+    //value pega o valor colocado dentro do input
+    // console.log(form.altura.value);
+   
+
+    //pega os dados informados nos campos de input e printa no console
+    
+    // var nome = form.nome.value;
+    // var peso = form.peso.value;
+    // var altura = form.altura.value;
+    // var gordura = form.gordura.value;
+    // console.log(nome);
+    // console.log(peso);
+    // console.log(altura);
+    // console.log(gordura);
+    
+    //Substituido pela função que se segue...
+
+    var paciente = obtemPacienteDoFormulario(form);
+    console.log(paciente);
+
+    
+    //cria um elemento desejado no html(criando uma linha na tabela)
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //atribui o conteudo da célula ao que foi posto no input lá na aplicação
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //utiliza a função criada em calcula-imc.js
+    imcTd.textContent = calculaImc(peso,altura);
+
+    //appendChild() adiciona elementos dentro de elementos hierarquicamente superiores
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+  
+
+    //acessa o id tabela-pacientes do html e adiciona um elemento dentro dele
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+
+});
+
+function obtemPacienteDoFormulario(form){
+    //criar um objeto em JS. Usa-se chaves e : da seguinte forma:
+    
+    var paciente = {
+        //propriedades do objeto são representadas com : e separados com ,
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    //retorna o valor dentro da variável paciente e encerra a função
+    return paciente;
+}
